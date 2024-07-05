15a30f9c11503b9c79a48daf9a2de875a15bde7c
esoji1 <angry8675t@gmail.com>
2024-07-05 20:39:22 +0300

    Spawn
    механик появление станков и работников

---
+++ idle My Factory Tycoon/Assets/Scripts/Spawn.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    private const int PRICE_MACHINE = 100;
+    private const int PRICE_WORKER = 60;
+
+    [SerializeField] private GameObject[] machineWorker;
+    [SerializeField] private GameObject[] button;
+
+    private Wallet wallet;
+
+    public void Start() {
+        wallet = GetComponent<Wallet>();
+    }
+
+    public void SpawnWorker(int index) {
+        if (wallet.SpendCoins(PRICE_WORKER)) {
+            machineWorker[index].SetActive(true);
+        }
+    }
+
+    public void SpawnMachine(int index) {
+        if (wallet.SpendCoins(PRICE_MACHINE)) {
+            machineWorker[index].SetActive(true);
+        }
+    }
+
+    public void Active(int indexMachine) {
+        if (wallet.GetCoins() >= PRICE_MACHINE || wallet.GetCoins() >= PRICE_WORKER) {
+            button[indexMachine].SetActive(false);
+        }
+    }
+}
