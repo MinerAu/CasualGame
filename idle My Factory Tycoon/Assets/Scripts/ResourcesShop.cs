76e7143f9ffd31e6106c97dc3fbc94d0ba7ade45
Mas632 <of.mas632@yandex.ru>
2024-07-21 22:27:30 +0300

    Add ResourceShop

---
+++ idle My Factory Tycoon/Assets/Scripts/ResourcesShop.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class ResourcesShop : MonoBehaviour
+{
+    [SerializeField] private Wallet _wallet;
+    [SerializeField] private Warehouse _warehouse;
+
+    public event UnityAction<string, int> ResourcesAmountChanged;
+
+    public void BuyResource(string resourceName)
+    {
+        Item resource = _warehouse.GetResource(resourceName);
+
+        if (resource == null)
+        {
+            Debug.Log($"No Resource With Name={resourceName}");
+            return;
+        }
+
+        if (_wallet.SpendCoins(resource.coinsItem))
+        {
+            resource.quantityItem++;
+            ResourcesAmountChanged?.Invoke(resourceName, resource.quantityItem);
+            ResourcesAmountChanged?.Invoke("coins", _wallet.GetCoins());
+        }
+    }
+}
---
+++ idle My Factory Tycoon/Assets/Scripts/ResourcesViewer.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourcesViewer : MonoBehaviour
+{
+    [SerializeField] private ResourcesShop _resourcesShop;
+
+    [SerializeField] private Text _coinsLeft;
+    [SerializeField] private Text _boardsLeft;
+    [SerializeField] private Text _boxOfNailsLeft;
+
+    private void OnEnable()
+    {
+        _resourcesShop.ResourcesAmountChanged += ShowResourceAmount;
+    }
+
+    private void OnDisable()
+    {
+        _resourcesShop.ResourcesAmountChanged -= ShowResourceAmount;
+    }
+
+    public void ShowResourceAmount(string resourceName, int resourceAmount)
+    {
+        switch (resourceName)
+        {
+            case "coins":
+                ShowCoinsAmount(resourceAmount);
+                break;
+
+            case "board":
+                ShowBoardsAmount(resourceAmount);
+                break;
+
+            case "box of nails":
+                ShowBoxOfNailsAmount(resourceAmount);
+                break;
+
+            default:
+                Debug.Log("Unknown resource name");
+                break;
+        }
+    }
+
+    private void ShowCoinsAmount(int amount)
+    {
+        _coinsLeft.text = $"{amount}$";
+    }
+
+    private void ShowBoardsAmount(int amount)
+    {
+        _boardsLeft.text = $"x{amount}";
+    }
+
+    private void ShowBoxOfNailsAmount(int amount)
+    {
+        _boxOfNailsLeft.text = $"x{amount}";
+    }
+}
