20a9e383e69efb4ef8f33bf5e061e0eb81296d20
Mas632 <of.mas632@yandex.ru>
2024-07-26 06:05:15 +0300

    ResourceShop
    Добавлены UI элементы под все ресурсы

--- idle My Factory Tycoon/Assets/Scripts/ResourcesViewer.cs
+++ idle My Factory Tycoon/Assets/Scripts/ResourcesViewer.cs
@@ -6,15 +6,45 @@ using UnityEngine.UI;
 
 public class ResourcesViewer : MonoBehaviour
 {
-    [SerializeField] private ResourcesShop _resourcesShop;
+    private string[] _resourceNames = new string[]
+    {
+        "board",
+        "box of nails",
+        "Metal Sheet",
+        "A box of screws",
+        "Metal Alloy",
+        "Rubber",
+        "Plastic",
+        "Electrical circuit"
+    };
 
+    [SerializeField] private ResourcesShop _resourcesShop;
     [SerializeField] private Text _coinsLeft;
-    [SerializeField] private Text _boardsLeft;
-    [SerializeField] private Text _boxOfNailsLeft;
+    [SerializeField] private TextMeshProUGUI _boardsLeft;
+    [SerializeField] private TextMeshProUGUI _boxOfNailsLeft;
+    [SerializeField] private TextMeshProUGUI _metalSheetsLeft;
+    [SerializeField] private TextMeshProUGUI _boxOfScrewsLeft;
+    [SerializeField] private TextMeshProUGUI _metalAlloyLeft;
+    [SerializeField] private TextMeshProUGUI _rubberLeft;
+    [SerializeField] private TextMeshProUGUI _plasticLeft;
+    [SerializeField] private TextMeshProUGUI _electricalCircuitLeft;
+
+    [SerializeField] private Warehouse _warehouse;
+    [SerializeField] private Wallet _wallet;
 
     private void OnEnable()
     {
         _resourcesShop.ResourcesAmountChanged += ShowResourceAmount;
+    }
+
+    private void Start()
+    {
+        foreach (string resourceName in _resourceNames)
+        {
+            ShowResourceAmount(resourceName, _warehouse.GetResource(resourceName).quantityItem);
+        }
+
+        ShowCoinsAmount(_wallet.GetCoins());
     }
 
     private void OnDisable()
@@ -38,6 +68,30 @@ public class ResourcesViewer : MonoBehaviour
                 ShowBoxOfNailsAmount(resourceAmount);
                 break;
 
+            case "Metal Sheet":
+                ShowMetalSheetsAmount(resourceAmount);
+                break;
+
+            case "A box of screws":
+                ShowBoxOfScrewsAmount(resourceAmount);
+                break;
+
+            case "Metal Alloy":
+                ShowMetalAlloyAmount(resourceAmount);
+                break;
+
+            case "Rubber":
+                ShowRubberAmount(resourceAmount);
+                break;
+
+            case "Plastic":
+                ShowPlasticAmount(resourceAmount);
+                break;
+
+            case "Electrical circuit":
+                ShowElectricalCircuitAmount(resourceAmount);
+                break;
+
             default:
                 Debug.Log("Unknown resource name");
                 break;
@@ -57,5 +111,35 @@ public class ResourcesViewer : MonoBehaviour
     private void ShowBoxOfNailsAmount(int amount)
     {
         _boxOfNailsLeft.text = $"x{amount}";
+    }
+
+    private void ShowMetalSheetsAmount(int amount)
+    {
+        _metalSheetsLeft.text = $"x{amount}";
+    }
+
+    private void ShowBoxOfScrewsAmount(int amount)
+    {
+        _boxOfScrewsLeft.text = $"x{amount}";
+    }
+
+    private void ShowMetalAlloyAmount(int amount)
+    {
+        _metalAlloyLeft.text = $"x{amount}";
+    }
+
+    private void ShowRubberAmount(int amount)
+    {
+        _rubberLeft.text = $"x{amount}";
+    }
+
+    private void ShowPlasticAmount(int amount)
+    {
+        _plasticLeft.text = $"x{amount}";
+    }
+
+    private void ShowElectricalCircuitAmount(int amount)
+    {
+        _electricalCircuitLeft.text = $"x{amount}";
     }
 }
